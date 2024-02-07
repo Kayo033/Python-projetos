5f1d609426575f8c5c63aa7ee623e3ad68764afc
Kayo_093 <110784831+Kayo033@users.noreply.github.com>
2024-02-06 22:06:50 -0300

    novas aulas

---
+++ aulas/aula_08a.py
@@ -0,0 +1,13 @@
+#ABAIXO IMPORTA TUDO DA BIBLIOTECA MATCH (MATEMATICA)
+# import math
+# a = float(input('Digite um numero:'))
+# raiz = math.sqrt(a)
+#
+# print('A raiz de {} é igual à {}'.format(a, raiz))
+
+#ABAIXO IMPORTA SOMENTE A FUNÇÃO SQRT (RAIZ DE UM NUMERO)
+from math import sqrt
+a = float(input('Digite um numero:'))
+raiz = sqrt(a)
+
+print('A raiz de {} é igual à {}'.format(a, raiz))
